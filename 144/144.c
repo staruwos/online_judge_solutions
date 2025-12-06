32fb306f8811e55d0b086344347a5b36020e49c6
staruwos <juliotrasferetti@gmail.com>
2025-12-06 06:35:09 -0300

    fix for 144

--- 144/144.c
+++ 144/144.c
@@ -1,70 +1,71 @@
 #include <stdio.h>
 
 typedef struct {
-    int id;
-    int grant;
-}Student;
+	int id;
+	int grant;
+} Student;
 
-int all_granted(Student students[], int length)
-{
+int all_granted(Student students[], int length) {
 	int i;
-	for(i = 0; i < length; i++)
-	{
+	for(i = 0; i < length; i++) {
 		if(students[i].grant < 40)
 			return 0;
-	}	
+	}
 	return 1;
 }
 
-int main()
-{
-    int N, k;
-    int i;
+int main() {
+	int N, k;
+	int i;
 
-    while(scanf("%d %d", &N, &k) != EOF)
-    {
-        Student students[N];
-
-	/* Initialize students list */
-	for(i = 0; i < N; i++)
+	while(scanf("%d %d", &N, &k) != EOF)
 	{
-		students[i].id = i+1;
-		students[i].grant = 0;
-	}
-	int current_coin = 1;
-	int students_left = N;
+		if (N == 0 && k == 0)
+			break;
 
-	while(all_granted(students, N) == 0)
-	{
-		for(i = 0; i < N; i++)
-		{
-			if(students[i].grant < 40)
-			{
-				if(students[i].grant + current_coin > 40)
-				{
-					current_coin = (students[i].grant + current_coin) - 40;
-					students[i].grant = 40;
-					printf("%d ", students[i].id);
-				}
-				else
-				{
-					students[i].grant += current_coin;
-					if(current_coin + 1 > k)
-						current_coin = 1;
-					else
-						current_coin++;
+		Student students[N];
 
-					if (students[i].grant == 40)
-						printf("%d ", students[i].id);
-				}
-
-			}
-
+		/* Initialize students list */
+		for(i = 0; i < N; i++) {
+			students[i].id = i + 1;
+			students[i].grant = 0;
 		}
-	
+
+		int k_counter = 1;
+		int output_store = 0;
+
+		while(all_granted(students, N) == 0)
+		{
+			for(i = 0; i < N; i++) {
+				if(students[i].grant < 40)
+				{
+					if (output_store == 0)
+					{
+						output_store = k_counter;
+
+						if (k_counter < k) {
+							k_counter++;
+						} else {
+							k_counter = 1;
+						}
+					}
+
+					int needed = 40 - students[i].grant;
+
+					if (output_store >= needed)
+					{
+						students[i].grant = 40;
+						output_store -= needed;
+
+						printf("%3d", students[i].id);
+					} else {
+						students[i].grant += output_store;
+						output_store = 0;
+					}
+				}
+			}
+		}
+		printf("\n");
 	}
-	
-		
-    }
-    return 0;
+	return 0;
 }
